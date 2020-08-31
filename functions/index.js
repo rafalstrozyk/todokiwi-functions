b93d2befe84036611fbadfadaadf4920e8de96b4
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-08-31 18:03:12 +0200

    add cors

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,17 @@
 const functions = require('firebase-functions');
-const { db } = require('./utils/admin');
 require('dotenv').config();
 
+const app = require('express')();
+
+const cors = require('cors');
 // firebase serve
-
-const serviceAccount = require('./key/key.json');
-
-const express = require('express');
-const app = express();
 
 const fbAuth = require('./utils/fbAuth');
 const { signup, login, logout } = require('./handlers/users');
 const { createColumn, getColumns, deleteColumn } = require('./handlers/userColumn');
 const { createTodo, getTodos,deleteTodo } = require('./handlers/todos');
+
+app.use(cors);
 
 // Users handle
 app.post('/signup', signup);
@@ -29,19 +28,5 @@ app.post('/todo', fbAuth, createTodo);
 app.get('/todos', fbAuth, getTodos);
 app.delete('/todo', fbAuth, deleteTodo);
 
-app.get('/todos', (req, res) => {
-	db.collection('todo')
-		.get()
-		.then((data) => {
-			let todos = [];
-			data.forEach((doc) => {
-				todos.push(doc.data());
-			});
-			return res.json(todos);
-		})
-		.catch((err) => {
-			console.error(err);
-		});
-});
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
